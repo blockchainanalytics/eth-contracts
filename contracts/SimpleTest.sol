23cc099d228329e8ad900765b454fc2a92b440f1
shahzad73 <shahzad73@hotmail.com>
2018-08-16 20:46:34 +0500

    First contract

---
+++ contracts/SimpleTest.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.18;
+
+contract SimpleTest {
+    
+  string public currentString;
+  uint8 public currentInt;
+
+  constructor(string cs, uint8 ci) public {
+    currentString = cs;
+    currentInt = ci;
+  }
+  
+
+  function setCurrentData(string cs, uint8 ci) public {
+    currentString = cs;
+    currentInt = ci;
+  }
+
+
+  function getCurrentStringData() view public returns (string)
+  {
+     return currentString;
+  }
+  
+  
+  function getCurrentIntData() view public returns (uint8)
+  {
+     return currentInt;
+  }
+  
+}
